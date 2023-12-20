98416517efa4138dc36f01531122761db5df1e6c
Trung Kien Luu <kienluu2703@icloud.com>
2023-12-20 22:07:52 +0700

    Adding functions

--- Cryptography-Project/website/myfirstapp/urls.py
+++ Cryptography-Project/website/myfirstapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from .views import signup, home, login_view, staff_profile, patient_profile
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('home/', home, name='home'),
+    path('signup/', signup, name='signup'),
+    path('login/', login_view, name='login'),
+    path('staff_profile/', staff_profile, name='staff_profile'),
+    path('patient_profile/', patient_profile, name='patient_profile'),
 ]
--- Cryptography-Project/website/myfirstapp/views.py
+++ Cryptography-Project/website/myfirstapp/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from pymongo import MongoClient
 import random
 from faker import Faker
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from .forms import SignUpForm
+# myfirstapp/views.py
+from django.shortcuts import render, redirect
+from pymongo import MongoClient
+from .forms import LoginForm
+from django.contrib.auth.hashers import check_password
+import re
 
 def get_db_handle(db_name, host, port, username, password):
     client = MongoClient(host=host,
@@ -39,6 +48,80 @@ def transfer_records(src_db_handle, dest_db_handle):
 def home(request):
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
+
 def template(request):
     template = loader.get_template('myfirst.html')
     return HttpResponse(template.render())
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            client = MongoClient('mongodb+srv://keandk:mongodb12@cluster0.hfwbqyp.mongodb.net/')
+            db = client['user']
+            log_and_auth = db['logAndAuth']
+
+            user_data = {
+                'username': form.cleaned_data.get('username'),
+                'password': form.cleaned_data.get('password'),
+            }
+
+            log_and_auth.insert_one(user_data)
+
+            return redirect('myfirstapp:home')
+    else:
+        form = SignUpForm()
+    template = loader.get_template('signup.html')
+    return HttpResponse(template.render({'form': form}, request))
+
+# myfirstapp/views.py
+def staff_profile(request):
+    user_data = request.session.get('user')
+
+    if user_data and 'staff' in user_data:
+        return render(request, 'staff_profile.html', {'user_data': user_data})
+    else:
+        return redirect('login')
+    
+def patient_profile(request):
+    user_data = request.session.get('user')
+
+    if user_data and 'patient' in user_data:
+        return render(request, 'patient_profile.html', {'user_data': user_data})
+    else:
+        return redirect('login')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            client = MongoClient('mongodb+srv://keandk:mongodb12@cluster0.hfwbqyp.mongodb.net/')
+            db = client['user']
+            users = db['logAndAuth']
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            user_data = users.find({'username': username})
+
+            for user in user_data:
+                stored_password = user['password']
+                stored_password = re.sub(r'\$', '', stored_password)  # Remove "$" character from stored password
+
+                if check_password(password, stored_password):
+                    request.session['user'] = user
+                    if 'user' in user:
+                        return redirect('myfirstapp:patient_profile')
+                    elif 'staff' in user:
+                        return redirect('myfirstapp:staff_profile')
+                    else:
+                        return redirect('myfirstapp:home')
+            
+            # If no matching username and password found
+            return redirect('myfirstapp:home')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
+
+# The check_password function in Django uses the PBKDF2 algorithm with a SHA-256 hash. 
+# It is the default password hashing algorithm used by Django for user authentication.
