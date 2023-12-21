99056a8ddc475fedb816af6212d2abcc7284fbcc
lanphuongnt <phuongntl.work@gmail.com>
2023-12-21 14:16:09 +0700

    changeview

--- Cryptography-Project/website/myfirstapp/views.py
+++ Cryptography-Project/website/myfirstapp/views.py
@@ -9,23 +9,6 @@ from django.contrib.auth import login, authenticate
 from .forms import SignUpForm, LoginForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.hashers import check_password, make_password
-from .source.mypackages.CA import CentralizedAuthority
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.template import loader
-from pymongo import MongoClient
-import random
-from faker import Faker
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from .forms import SignUpForm, LoginForm
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth.hashers import check_password, make_password
-
-
-
-server_CA = CentralizedAuthority()
-connection_string = 'mongodb+srv://lanphuongnt:keandk27@cluster0.hfwbqyp.mongodb.net/'
 
 def get_db_handle(db_name, host, port, username, password):
     client = MongoClient(host=host,
@@ -70,7 +53,7 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            client = MongoClient(connection_string)
+            client = MongoClient('mongodb+srv://keandk:mongodb12@cluster0.hfwbqyp.mongodb.net/')
             db = client['user']
             log_and_auth = db['logAndAuth']
 
@@ -85,9 +68,7 @@ def signup(request):
             }
 
             log_and_auth.insert_one(user_data)
-            user = log_and_auth.find_one({'username', username})
-            # set tam cai ID
-            server_CA.Setup(user['_id'])
+
             return redirect('myfirstapp:home')
     else:
         form = SignUpForm()
@@ -111,37 +92,43 @@ def patient_profile(request):
     else:
         return redirect('login')
 
-
 def login_view(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            client = MongoClient(connection_string)
+            client = MongoClient('mongodb+srv://keandk:mongodb12@cluster0.hfwbqyp.mongodb.net/')
             db = client['user']
-            collection = db['logAndAuth']
+            users = db['logAndAuth']
 
             username = form.cleaned_data.get('username')
-            password = hashlib.sha3_512(form.cleaned_data.get('password').encode()).hexdigest()
+            password = form.cleaned_data.get('password')
 
-            user = collection.find_one({'username': username})
-            # HttpResponse(str(user))
-            if user is None:
-                return redirect('myfirstapp:home')
-            else:
+            user_data = users.find({'username': username})
+
+            for user in user_data:
                 stored_password = user['password']
                 if check_password(password, stored_password):
                     request.session['user'] = user
-                    if user['role'] == 'user':
+                    if 'user' in user:
                         return redirect('myfirstapp:patient_profile')
-                    elif user['role'] == 'staff':
+                    elif 'staff' in user:
                         return redirect('myfirstapp:staff_profile')
                     else:
                         return redirect('myfirstapp:home')
-
+            
             # If no matching username and password found
+            return redirect('myfirstapp:home')
     else:
         form = LoginForm()
     return render(request, 'login.html', {'form': form})
 
 # The check_password function in Django uses the PBKDF2 algorithm with a SHA-256 hash. 
 # It is the default password hashing algorithm used by Django for user authentication.
+
+
+
+
+# log_and_auth.insert_one(user_data)
+#             user = log_and_auth.find_one({'username', username})
+#             # set tam cai ID # 
+            # server_CA.Setup(user['_id'])
