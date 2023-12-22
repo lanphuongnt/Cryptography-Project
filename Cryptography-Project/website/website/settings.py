fab8c52dfbc1a630b7b0ff549860d569fc72779e
lanphuongnt <phuongntl.work@gmail.com>
2023-12-22 20:40:52 +0700

    add theme folder

--- Cryptography-Project/website/website/settings.py
+++ Cryptography-Project/website/website/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'myfirstapp/templates/static',
+    BASE_DIR / 'myfirstapp/static',
 ]
+
+STATIC_ROOT = BASE_DIR / 'myfirstapp/static_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
