4aa83c843dd2a90dd39b6169c50eacdf34537686
Kien Luu <kienluu2703@icloud.com>
2023-12-23 15:20:29 +0700

    Handled EHR and Staff info saving to DB

--- Cryptography-Project/website/myfirstapp/views.py
+++ Cryptography-Project/website/myfirstapp/views.py
@@ -2,75 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from pymongo import MongoClient
-import random
-from django.contrib.auth.models import User
-from django.contrib.auth import login, authenticate
-from .forms import SignUpForm, LoginForm
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.hashers import check_password, make_password
-from django.contrib.auth.decorators import login_required
 
-from .source.mypackages.CA import CentralizedAuthority
-from django.contrib import messages
-from django.contrib.auth import login
 from bson import ObjectId
 from django.views.decorators.cache import never_cache
-from flatten_json import flatten, unflatten
-import json
+from .source.mypackages.CA import CentralizedAuthority
+from.utils import create_new_EHR, get_data, insert_data
 
 server_CA = CentralizedAuthority()
 # server_CA.AddPolicy()
-
-
-def create_new_EHR(request, userID):
-    status = request.POST['status']
-    if status == 'Patient':
-        addition = {
-                "patient_info": {
-                    "name": request.POST['name'],
-                    "dob": "",
-                    "gender": "",
-                    "cccd": "",
-                    "contact": {
-                        "phone": "",
-                        "email": ""
-                    },
-                    "address": {
-                        "street": "",
-                        "city": "",
-                        "state": "",
-                        "zip": ""
-                    }
-                }
-            } 
-    else:
-        addition = {
-            "staff_info": {
-                "name": request.POST['name'],
-                "dob": "",
-                "gender": "",
-                "role": status,
-                "position": "",
-                "contact": {
-                    "phone": "",
-                    "email": ""
-                },
-                "address": {
-                    "street": "",
-                    "city": "",
-                    "state": "",
-                    "zip": ""
-                }
-            }
-        }
-    addition['_id'] = userID
-
-    db = server_CA.client['data']
-    if status == 'Patient':
-        ehr_col = db['ehr']
-    else:
-        ehr_col = db['staff']
-    ehr_col.insert_one(addition)
 
 def index(request):
     template = loader.get_template('myfirst.html')
@@ -164,14 +104,6 @@ def forgot_password(request):
 def staff_profile(request):
     user = request.session['user']
 
-    # Check if the user's role is 'staff'
-    if user['role'] != 'staff':
-        # If not, redirect them to their correct profile or home page
-        if user['role'] == 'user':
-            return redirect('myfirstapp:patient_profile')
-        else:
-            return redirect('myfirstapp:index')
-
     user_id = str(user['_id'])
     template = loader.get_template('staff_profile.html')
     return HttpResponse(template.render({'user_id': user_id}, request))
@@ -180,82 +112,16 @@ def staff_profile(request):
 @custom_login_required
 def patient_profile(request):
     user = request.session['user']
+    # user_id = str(user['_id'])
+    # client = MongoClient('mongodb+srv://keandk:mongodb12@cluster0.hfwbqyp.mongodb.net/')
+    # collection = client['data']['ehr']
+    # patient_info = collection.find_one({'_id': ObjectId(user_id)})
 
     # Check if the user's role is 'user'
-    if user['role'] != 'user':
-        # If not, redirect them to their correct profile or index page
-        if user['role'] == 'staff':
-            return redirect('myfirstapp:staff_profile')
-        else:
-            return redirect('myfirstapp:index')
-
-    user_id = str(user['_id'])
-    template = loader.get_template('users-profile copy.html')
-    return HttpResponse(template.render({'user_id': user_id}, request))
+    # if user['role'] != 'user':
+    # template = loader.get_template('users-profile copy.html')
+    return render(request, 'users-profile copy.html')
+    # return HttpResponse(template.render({'patient_info': patient_info}, request))
 # The check_password function in Django uses the PBKDF2 algorithm with a SHA-256 hash. 
 # It is the default password hashing algorithm used by Django for user authentication.
-
-
-def insert_data(request):
-    # Request (dict) include: database, collection, username(ObjectID), {'$set' : {'dataname1': datavalue1}, {data}}
-    # Example : request = {'database' : 'data', 'collection' : 'ehr', 'username' : '65845045be5cf517d0a932e1', {'height' : 153}}
-    db = server_CA.client[request['database']]
-    collection = db[request['collection']]
-
-    update_data = request['$set']
-    encrypted_data = {}
-    flatten(update_data)
-
-    policy_col = server_CA.client['policy_repository']['abe']
-    policy = policy_col.find_one({'request' : 'insert'})['policy']
-
-    # CA_db = server_CA.client['CA']
-    # attribute_col = CA_db['subject_attribute']
-    # user_attribute = attribute_col.find_one({"_id" : ObjectId(request['username'])})    
-    
-    # private_key, public_key = server_CA.GeneratePrivateKey(request['username'], user_attribute)
-    public_key = server_CA.GetPublicKey(request['username'])
-
-    for data in update_data.item():
-        encrypted_data[data.key()] = server_CA.cpabe.encrypt(public_key, data.value, policy)
-
-    unflatten(encrypted_data)
-
-    collection.update_many({'_id': ObjectId(request['username'])}, {'$set': encrypted_data})
-    response = collection.find_one({'_id': ObjectId(request['username'])})
-    if response:
-        return True
-    else:
-        return False
-    
-def get_data(request):
-    # Request (dict) include: database, collection, username(ObjectID), {'$get' : {'dataname1': datavalue1}, {data}}
-    # Example : request = {'database' : 'data', 'collection' : 'ehr', 'username' : '65845045be5cf517d0a932e1', {'height' : 153}}
-    db = server_CA.client[request['database']]
-    collection = db[request['collection']]
-    encryted_data = collection.find(request['$get'])
-
-    if encryted_data:
-        flatten(encryted_data)
-        recovered_data = {}
-        CA_db = server_CA.client['CA']
-        attribute_col = CA_db['subject_attribute']
-        user_attribute = attribute_col.find_one(request['$get'])    
-
-        private_key = server_CA.GeneratePrivateKey(request['username'], user_attribute)
-        public_key = server_CA.GetPublicKey(request['username'])
-
-        for ed in encryted_data:
-            recovered_data[ed.key()] = server_CA.cpabe.decrypt(public_key, ed.value(), private_key)
-
-        unflatten(recovered_data)
-
-        return recovered_data
-    else:
-        return None
-    
-def GetSubjectAttribute(self, userID, attribute_name): # attribute name is a list string 
-    # Load public key 
-    attribute = {} 
-    return attribute
-    
+        
