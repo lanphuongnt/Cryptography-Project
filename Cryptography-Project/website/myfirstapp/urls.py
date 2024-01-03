05b381e9325527fc1ee64b6bf8703a600ecd652b
lanphuongnt <phuongntl.work@gmail.com>
2024-01-03 16:40:03 +0700

    merge urls, doctor

--- Cryptography-Project/website/myfirstapp/urls.py
+++ Cryptography-Project/website/myfirstapp/urls.py
@@ -15,16 +15,10 @@ urlpatterns = [
     path('staff_profile/reference/patient_view/', views.ehr_view, name='patient_view'),
     path('get_patient_info/', views.get_patient_info, name='get_patient_info'),
     # path('staff_profile/reference/', views.reference_by_specialty, name='reference'),
-<<<<<<< HEAD
-    path('lanphuong/', views.Doctor, name='lanphuong'),
-    path('lanphuong/filter/', views.GetListOfPatientsWithFilter, name='filter'),
-    path('lanphuong/patient_ehr/', views.PatientHealthRecord, name='patient_ehr')
-=======
     path('doctor/', views.doctor, name='doctor'),
     path('filter/', views.GetListOfPatientsWithFilter, name='filter'),
     # path('lanphuong/', views.Doctor, name='lanphuong'),
     path('lanphuong/filter', views.GetListOfPatientsWithFilter, name='filter'),
     path('lanphuong/patient_ehr', views.ShowPatientHealthRecord, name='patient_ehr')
->>>>>>> ac68563bee3c05a76a8b39afb2be11f04980ddc5
     # path('lanphuongresult/', views.result, name='result'),
 ]
